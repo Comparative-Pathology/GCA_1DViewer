b676dbad3d7c7898d6248a11d366c10643861a02
mehran6 <mehransharghi@yahoo.co.uk>
2023-01-24 01:02:37 +0000

    Adding code to load help file and images

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
+
 import './css/GCA_1DViewer.css'
 import './modules/GCA_Utilities/external/jquery-ui/jquery-ui.min.css';
 import './modules/GCA_Utilities/src/GCA_Utilities.css';
+import './help/help.css';
 
+import rawHelpContent from './help/help.md'
 
 import anatomyData from './gca_anatomy_h_v1.json'
 
+window.rawHelpContent = rawHelpContent;
 window.viewer1dIcons = importAll(require.context('./Icons', false, /\.svg$/));
 window.jsonAnatomy = anatomyData;
+window.helpImages = importAll(require.context('./help/images', false, /\.png$/));
 window.cssImported = true;
 
 function importAll(r) {
   let images = {};
-  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+//  r.keys().forEach((key) => { images[key.replace('./', '')] = r(key); });
+  r.keys().map((item) => { images[item.replace('./', '')] = r(item); });
   return images;
 }
 
 export { Viewer1D } from './Viewer1D.js'
-
-
